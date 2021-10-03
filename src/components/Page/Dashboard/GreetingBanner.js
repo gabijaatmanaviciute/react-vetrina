a677a05da620ee292b9e18edc225dd29557ccb5e
Gabija Atm <gab.atmanaviciute@gmail.com>
2021-10-03 10:36:42 +0300

    greeting banner draft

--- src/components/Page/Dashboard/GreetingBanner.js
+++ src/components/Page/Dashboard/GreetingBanner.js
@@ -1,5 +1,8 @@
-import { Typography } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import { ReactComponent as ExternalLinkIcon } from "./../../../assets/icons/external-link-icon.svg";
 
 const GreetingBanner = () => {
   const classes = useStyles();
@@ -7,7 +10,12 @@ const GreetingBanner = () => {
   return (
     <div className={classes.greetingBanner}>
       <Typography className={classes.greetingText}>Welcome Mario!</Typography>
-      <Typography className={classes.greetingLink} component="a">app.vetrinalive.it/fenoh-store</Typography>
+      <Typography className={classes.greetingLink}>
+        <Link className={classes.linkText}>app.vetrinalive.it/fenoh-store</Link>
+        <SvgIcon className={classes.linkIcon}>
+          <ExternalLinkIcon />
+        </SvgIcon>
+      </Typography>
     </div>
   );
 };
@@ -16,18 +24,38 @@ export default GreetingBanner;
 
 const useStyles = makeStyles({
   greetingBanner: {
+    display: "flex",
+    alignContent: "center",
     position: "relative",
     height: "11.375rem",
     top: "3.75rem",
-    background: "linear-gradient(180deg, #21B8F9 0%, rgba(33, 184, 249, 0) 132.05%)",
-    padding: "2.5rem 2.3125rem"
+    background:
+      "linear-gradient(180deg, #21B8F9 0%, rgba(33, 184, 249, 0) 132.05%)",
+    padding: "2.5rem 2.3125rem",
   },
   greetingText: {
+    flex: 1,
     fontFamily: "Source Sans Pro",
     fontStyle: "normal",
     fontWeight: 600,
     fontSize: "2.125rem",
     lineHeight: "2.4375rem",
-    color: "#FFFFFF"
-  }
+    color: "#FFFFFF",
+  },
+  greetingLink: {
+    display: "flex",
+    color: "#FFFFFF",
+  },
+  linkText: {
+    color: "#FFFFFF",
+    fontFamily: "Noto Sans HK",
+    fontStyle: "normal",
+    fontWeight: 500,
+    fontSize: "1.0625rem",
+    lineHeight: "1.375rem",
+    textDecoration: "underline"
+  },
+  linkIcon: {
+    color: "#FFFFFF",
+  },
 });
