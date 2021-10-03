5975f9676bf9d3b248cfc3f1a0fdb282c1033389
Gabija Atm <gab.atmanaviciute@gmail.com>
2021-10-03 17:14:25 +0300

    menu data file added

---
+++ src/data-layer/menu-data.js
@@ -0,0 +1,100 @@
+export const mainListItems = [
+  {
+    icon_source: "/src/assets/icons/",
+    hover_label: "Dashboard",
+    name: "Dashboard",
+  },
+  {
+    icon_source: "/src/assets/icons/",
+    hover_label: "Catalogue",
+    name: "Catalogue",
+    sub_list: ["New Collection", "Discounted"],
+  },
+  { icon_source: "/src/assets/icons/", hover_label: "Orders", name: "Orders" },
+  {
+    icon_source: "/src/assets/icons/",
+    hover_label: "Customers",
+    name: "Customers",
+  },
+  {
+    icon_source: "/src/assets/icons/",
+    hover_label: "Marketing",
+    name: "Marketing",
+    sub_list: [
+      "Discount codes",
+      "Exit intent",
+      "Checkout Features",
+      "Post-purchase conversion",
+      "Cart abandonment",
+      "Timer checkout",
+      "Sell on Social",
+      "Special Offer",
+      "Seasonal Offer"
+    ],
+  },
+  {
+    icon_source: "/src/assets/icons/",
+    hover_label: "Delivery Options",
+    name: "Delivery Options",
+  },
+  {
+    icon_source: "/src/assets/icons/",
+    hover_label: "Payment Options",
+    name: "Payment Options",
+  },
+  {
+    icon_source: "/src/assets/icons/",
+    hover_label: "Payment Options",
+    name: "Payment Options",
+  },
+  {
+    icon_source: "/src/assets/icons/",
+    hover_label: "Store Design",
+    name: "Store Design",
+  },
+  {
+    icon_source: "/src/assets/icons/",
+    hover_label: "Subscription",
+    name: "Subscription",
+  },
+  {
+    icon_source: "/src/assets/icons/",
+    hover_label: "Integrations",
+    name: "Integrations",
+  },
+  {
+    icon_source: "/src/assets/icons/",
+    hover_label: "Extensions",
+    name: "Extensions",
+  },
+  {
+    icon_source: "/src/assets/icons/",
+    hover_label: "Settings",
+    name: "Settings",
+  },
+  {
+    icon_source: "/src/assets/icons/",
+    hover_label: "Log out",
+    name: "Log out",
+  },
+];
+
+export const extraListItems = [
+  {
+    icon_source: "/src/assets/icons/",
+    hover_label: "Customer Support",
+    name: "Customer Support",
+  },
+  {
+    icon_source: "/src/assets/icons/",
+    hover_label: "Share your shop",
+    name: "Share your shop",
+  },
+  {
+    icon_source: "/src/assets/icons/",
+    hover_label: "View your shop",
+    name: "View your shop",
+  },
+];
+
+export const storeList = ["Fenoh Store", "Alibaba Store"];
