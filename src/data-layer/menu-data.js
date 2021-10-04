a7cc29765623962e8e1acbe066073e333e84dc6e
Gabija Atm <gab.atmanaviciute@gmail.com>
2021-10-04 15:37:14 +0300

    menu data list updated

--- src/data-layer/menu-data.js
+++ src/data-layer/menu-data.js
@@ -1,23 +1,23 @@
 export const mainListItems = [
   {
-    icon_source: "/src/assets/icons/",
+    icon_title: "home-icon.svg",
     hover_label: "Dashboard",
     name: "Dashboard",
   },
   {
-    icon_source: "/src/assets/icons/",
+    icon_title: "shopping-cart-icon.svg",
     hover_label: "Catalogue",
     name: "Catalogue",
     sub_list: ["New Collection", "Discounted"],
   },
-  { icon_source: "/src/assets/icons/", hover_label: "Orders", name: "Orders" },
+  { icon_title: "list-icon.svg", hover_label: "Orders", name: "Orders" },
   {
-    icon_source: "/src/assets/icons/",
+    icon_title: "user-icon.svg",
     hover_label: "Customers",
     name: "Customers",
   },
   {
-    icon_source: "/src/assets/icons/",
+    icon_title: "target-icon.svg",
     hover_label: "Marketing",
     name: "Marketing",
     sub_list: [
@@ -29,51 +29,46 @@ export const mainListItems = [
       "Timer checkout",
       "Sell on Social",
       "Special Offer",
-      "Seasonal Offer"
+      "Seasonal Offer",
     ],
   },
   {
-    icon_source: "/src/assets/icons/",
+    icon_title: "truck-icon.svg",
     hover_label: "Delivery Options",
     name: "Delivery Options",
   },
   {
-    icon_source: "/src/assets/icons/",
+    icon_title: "dollar-sign-icon.svg",
     hover_label: "Payment Options",
     name: "Payment Options",
   },
   {
-    icon_source: "/src/assets/icons/",
-    hover_label: "Payment Options",
-    name: "Payment Options",
-  },
-  {
-    icon_source: "/src/assets/icons/",
+    icon_title: "brush-icon.svg",
     hover_label: "Store Design",
     name: "Store Design",
   },
   {
-    icon_source: "/src/assets/icons/",
+    icon_title: "credit-card-icon.svg",
     hover_label: "Subscription",
     name: "Subscription",
   },
   {
-    icon_source: "/src/assets/icons/",
+    icon_title: "cog-planet-icon.svg",
     hover_label: "Integrations",
     name: "Integrations",
   },
   {
-    icon_source: "/src/assets/icons/",
+    icon_title: "new-app-icon.svg",
     hover_label: "Extensions",
     name: "Extensions",
   },
   {
-    icon_source: "/src/assets/icons/",
+    icon_title: "settings.icon.svg",
     hover_label: "Settings",
     name: "Settings",
   },
   {
-    icon_source: "/src/assets/icons/",
+    icon_title: "log-out-icon.svg",
     hover_label: "Log out",
     name: "Log out",
   },
@@ -81,17 +76,17 @@ export const mainListItems = [
 
 export const extraListItems = [
   {
-    icon_source: "/src/assets/icons/",
+    icon_title: "help-circle-icon.svg",
     hover_label: "Customer Support",
     name: "Customer Support",
   },
   {
-    icon_source: "/src/assets/icons/",
+    icon_title: "share-2-icon.svg",
     hover_label: "Share your shop",
     name: "Share your shop",
   },
   {
-    icon_source: "/src/assets/icons/",
+    icon_title: "eye-icon.svg",
     hover_label: "View your shop",
     name: "View your shop",
   },
