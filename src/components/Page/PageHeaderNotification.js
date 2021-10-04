04c1b79eca81f1a7b3bdb0d7bea5d380e3742584
Gabija Atm <gab.atmanaviciute@gmail.com>
2021-10-05 02:48:17 +0300

    notification header fix

--- src/components/Page/PageHeaderNotification.js
+++ src/components/Page/PageHeaderNotification.js
@@ -1,20 +1,17 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import SvgIcon from "@material-ui/core/SvgIcon";
 import Badge from "@material-ui/core/Badge";
 import { makeStyles } from "@material-ui/core";
-import { ReactComponent as ZapIcon } from "./../../assets/icons/zap-icon.svg";
+import zapIcon from "assets/icons/zap-icon.svg";
 
 const HeaderNotification = () => {
   const classes = useSyles();
 
   return (
     <Typography className={classes.HeaderNotification}>
-      <Badge badgeContent={2} color="secondary">
-      <SvgIcon>
-        <ZapIcon />
-      </SvgIcon>
-        <Typography>What's new</Typography>
+      <Badge badgeContent={2} color="secondary" className={classes.notificationBadge}>
+        <img src={zapIcon} alt="Zap Icon" className={classes.zapIcon} />
+        <Typography variant="h6" className={classes.notificationText}>What's new</Typography>
       </Badge>
     </Typography>
   );
@@ -22,14 +19,20 @@ const HeaderNotification = () => {
 
 export default HeaderNotification;
 
-const useSyles = makeStyles({
+const useSyles = makeStyles(theme => ({
   HeaderNotification: {
     color: "#103B66",
   },
-  imageIcon: {
-    height: "100%",
+  zapIcon: {
+    height: "1.25rem",
+    marginRight: "7px"
+  },
+  notificationText: {
+    ...theme.typography.h6,
+    fontFamily: "Source Sans Pro",
+    fontWeight: 400,
   },
   iconRoot: {
     textAlign: "center",
   },
-});
+}));
