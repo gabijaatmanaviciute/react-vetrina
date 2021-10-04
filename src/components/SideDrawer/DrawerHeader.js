9a68cbe5a87d765b24ff7bd6a7e9401deb8deffd
Gabija Atm <gab.atmanaviciute@gmail.com>
2021-10-05 02:51:48 +0300

    drawer list style and logic enhanced

--- src/components/SideDrawer/DrawerHeader.js
+++ src/components/SideDrawer/DrawerHeader.js
@@ -1,17 +1,16 @@
 import React from "react";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import { ReactComponent as MainLogo } from "./../../assets/logo/main-logo.svg";
-import SvgIcon from "@material-ui/core/SvgIcon";
+import {ReactComponent as MenuIcon} from "assets/icons/menu-icon.svg";
+import mainLogo from "assets/logo/main-logo.svg";
 import { makeStyles } from "@material-ui/core";
 
-function DrawerHeader() {
+function DrawerHeader({changeMenuVisibility}) {
   const classes = useStyles();
 
   return (
     <div className={classes.drawerHeader}>
-      <MainLogo />
-      <IconButton className={classes.menuButton}>
+      <img className={classes.mainLogo} alt="Site logo" src={mainLogo} />
+      <IconButton className={classes.menuButton} onClick={changeMenuVisibility}>
         <MenuIcon />
       </IconButton>
     </div>
@@ -20,14 +19,28 @@ function DrawerHeader() {
 
 export default DrawerHeader;
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   drawerHeader: {
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
-    padding: "1.5rem 1.25rem",
+    padding: "1.5rem 1.5rem 1.5rem 1.25rem",
+    position: "sticky",
+    top: 0,
+    background: theme.palette.background.default,
+    zIndex: 10,
+  },
+  mainLogo: {
+    "&:hover": {
+      cursor: "pointer",
+    },
   },
   menuButton: {
-      padding: 0,
+    height: "1.5rem",
+    width: "1.5rem",
+    padding: 0,
+    "&:hover": {
+      background: "transparent",
+    }
   },
-});
+}));
--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,21 +1,29 @@
+import { useState } from "react";
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
 import { makeStyles } from "@material-ui/core/styles";
 import DrawerHeader from "./DrawerHeader";
+import { Link } from "react-router-dom";
 
-import {mainListItems} from "./../../data-layer/menu-data";
-import {extraListItems} from "./../../data-layer/menu-data";
-import {storeList} from "./../../data-layer/menu-data";
+import { mainListItems } from "./../../data-layer/menu-data";
+import { extraListItems } from "./../../data-layer/menu-data";
+import { storeList } from "./../../data-layer/menu-data";
+import { ReactComponent as HomeIcon } from "assets/icons/home-icon.svg";
 
 const drawerWidth = 255;
 
 const SideDrawer = () => {
+  const [showMenu, setShowMenu] = useState(true);
+  const [selectedItem, setSelectedItem] = useState("dashboard");
   const classes = useStyles();
+
+  const menuIconClickHandler = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
     <Drawer
@@ -24,43 +32,105 @@ const SideDrawer = () => {
       className={classes.drawer}
       classes={{ paper: classes.drawerPaper }}
     >
-      <DrawerHeader />
-      <List>
-        {mainListItems.map( item => (
-          <ListItem button key={item.name}>
-            <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon>
-            <ListItemText primary={item.name} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {extraListItems.map(item => (
-          <ListItem button key={item.name}>
-            <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon>
-            <ListItemText primary={item.name} />
-          </ListItem>
-        ))}
-      </List>
+      <DrawerHeader changeMenuVisibility={menuIconClickHandler} />
+      
+        <List className={classes.menuList}>
+          {mainListItems.map((item) => (
+            <ListItem
+              disableGutters
+              button
+              onClick={() => setSelectedItem(item.destination)}
+              component={Link}
+              to={`/${item.destination}`}
+              key={item.name}
+              className={`classes.menuListItem ${
+                selectedItem === item.destination ? "selected" : ""
+              }`}
+            >
+              <div className={classes.selectedItemBar} />
+              
+              <ListItemIcon className={classes.listItemIcon}>
+                <img src={require(`assets/icons/${item.icon_title}`).default} />
+                {/* <HomeIcon className={classes.homeIcon} /> */}
+              </ListItemIcon>
+              <ListItemText disableTypography className={classes.listItemText}>
+                {item.name}
+              </ListItemText>
+              
+            </ListItem>
+          ))}
+        </List>
+      
+      <Divider/>
+      <List className={classes.menuList}>
+          {extraListItems.map((item) => (
+            <ListItem
+              disableGutters
+              button
+              onClick={() => setSelectedItem(item.destination)}
+              component={Link}
+              to={`/${item.destination}`}
+              key={item.name}
+              className={`classes.menuListItem ${
+                selectedItem === item.destination ? "selected" : ""
+              }`}
+            >
+              <div className={classes.selectedItemBar} />
+              
+              <ListItemIcon className={classes.listItemIcon}>
+                <img src={require(`assets/icons/${item.icon_title}`).default} />
+              </ListItemIcon>
+              <ListItemText disableTypography className={classes.listItemText}>
+                {item.name}
+              </ListItemText>
+              
+            </ListItem>
+          ))}
+        </List>
     </Drawer>
   );
 };
 
 export default SideDrawer;
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   drawer: {
     width: drawerWidth,
-    // height: "100vh",
-    // display: "flex",
-    // flexDirection: "column",
-    background: "#0000FF",
   },
   drawerPaper: {
     width: drawerWidth,
   },
-});
+  menuList: {
+    padding: 0,
+    margin: "0.1875rem 0 1rem",
+    maxWidth: drawerWidth,
+  },
+  selectedItemBar: {
+    display: "inline-block",
+    width: "4px",
+    height: "2rem",
+    marginRight: "3px",
+    borderRadius: "4px",
+  },
+  menuListItem: {
+    padding: "0.375rem 1rem 0.375rem 0.8125rem",
+    height: "2rem",
+    borderRadius: "4px",
+    "&:hover, &:active": {},
+  },
+  listItemIcon: {
+    marginLeft: "0.8125rem",
+    marginRight: "0.8125rem",
+    minWidth: "1.25rem",
+    color: theme.palette.common.red
+  },
+  listItemText: {
+    padding: 0,
+    margin: 0,
+    color: theme.palette.common.black,
+    fontFamily: "Noto Sans HK",
+    fontWeight: 400,
+    fontSize: "0.875rem",
+    lineHeight: "1rem",
+  },
+}));
