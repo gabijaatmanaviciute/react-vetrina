3b049c48f7ef302e0416e7c9e7f9a7be5fc36658
Gabija Atm <gab.atmanaviciute@gmail.com>
2021-10-06 00:30:54 +0300

    panel card custom theme

--- src/components/UI/Theme.js
+++ src/components/UI/Theme.js
@@ -11,9 +11,14 @@ const sunflower = "#FFCF5C";
 const green = "#00C48C";
 const lightBlue = "#E9F8FE";
 const lightGrey = "#E5E5E5";
-const hoverGrey = "background: #F7F7F7";
+const hoverGrey = "#F7F7F7";
 
 export default createTheme({
+  panelCard: {
+    boxShadow: "0px 4px 8px 0px rgba(50, 50, 71, 0.06),0px 4px 4px 0px rgba(50, 50, 71, 0.08)",
+    borderRadius: "10px",
+    padding: "1.5rem",
+  },
   props: {
     // Name of the component
     MuiButtonBase: {
@@ -69,42 +74,36 @@ export default createTheme({
       fontSize: "2.125rem",
       fontWeight: 500,
       lineHeight: "2.4375rem",
-
     },
     h2: {
       fontFamily: "Noto Sans HK",
       fontWeight: 500,
       fontSize: "1.75rem",
       lineHeight: "2rem",
-
     },
     h3: {
       fontFamily: "Noto Sans HK",
       fontWeight: 500,
       fontSize: "1.375rem",
       lineHeight: "1.6875rem",
-
     },
     h4: {
       fontFamily: "Noto Sans HK",
       fontWeight: 500,
       fontSize: "1.25rem",
       lineHeight: "1.5625rem",
-
     },
     h5: {
       fontFamily: "Noto Sans HK",
       fontWeight: 500,
       fontSize: "1.0625rem",
       lineHeight: "1.375rem",
-
     },
     h6: {
       fontFamily: "Noto Sans HK",
       fontWeight: 500,
       fontSize: "0.9375rem",
       lineHeight: "1.25rem",
-
     },
     bodyMedium: {
       fontWeight: 500,
@@ -127,14 +126,20 @@ export default createTheme({
       fontWeight: 400,
       fontSize: "0.875rem",
       lineHeight: "1.125rem",
-
     },
     label: {
       fontFamily: "Source Sans Pro",
       fontWeight: 500,
       fontSize: "0.9375rem",
       lineHeight: "1.25rem",
-
     },
+    panelLinks: {
+      fontFamily: "Source Sans Pro",
+      fontWeight: 400,
+      fontSize: "1.0625rem",
+      lineHeight: "1.25rem",
+      color: `${blue}`,
+      textDecoration: "underline"
+    }
   },
 });
