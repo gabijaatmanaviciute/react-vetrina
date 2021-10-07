769d6e93ee350daccf53a5b63d39fb4fe967a1ac
Gabija Atm <gab.atmanaviciute@gmail.com>
2021-10-07 09:02:55 +0300

    card header comp draft

---
+++ src/components/Page/Dashboard/PanelCardParts/CardHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CardTitle from "./CardTitle";
+import 
+
+function CardHeader({cardTitle, sideElement}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CardHeader
