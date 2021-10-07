89adeb53d655bbb9216aeed34527a2f8d8176680
Gabija Atm <gab.atmanaviciute@gmail.com>
2021-10-07 09:08:43 +0300

    visitors comp style

--- src/components/Page/Dashboard/PanelCards/Visitors.js
+++ src/components/Page/Dashboard/PanelCards/Visitors.js
@@ -4,7 +4,9 @@ import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDownRounded";
 import ArrowForwardRoundedIcon from "@material-ui/icons/ArrowForwardRounded";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core";
+import { CardContent, makeStyles } from "@material-ui/core";
+import CardTitle from "../PanelCardParts/CardTitle";
+import CardLink from "../PanelCardParts/CardLink";
 
 function Visitors() {
   const classes = useStyles();
@@ -12,22 +14,22 @@ function Visitors() {
   return (
     <Grid container direction="column">
       <Grid item className={classes.panelHeader}>
-        <div className={classes.panelTitle}>
-          <EyeIcon className={classes.panelIcon} />
-          <Typography variant="h4">Visitors</Typography>
-        </div>
+        <CardTitle title="Visitors" icon={<EyeIcon />} />
         <div className={classes.dropdown}>
           <Typography variant="input">This month</Typography>
           <KeyboardArrowDownIcon />
         </div>
-        
       </Grid>
       <Grid item>
-        <Typography className={classes.panelContent}>1824</Typography>
+        <CardContent>
+          <Typography className={classes.panelContent}>1824</Typography>
+        </CardContent>
       </Grid>
       <Grid item className={classes.panelFooter}>
-        <Typography>Do you want more visits? Contact us!</Typography>
-        <ArrowForwardRoundedIcon className={classes.linkIcon} />
+        <CardLink
+          linkText="Do you want more visits? Contact us!"
+          linkIcon={<ArrowForwardRoundedIcon />}
+        />
       </Grid>
     </Grid>
   );
@@ -36,15 +38,6 @@ function Visitors() {
 export default Visitors;
 
 const useStyles = makeStyles((theme) => ({
-  panelIcon: {
-    width: "1.25rem",
-    height: "1.25rem",
-    marginRight: "1rem",
-  },
-  panelTitle: {
-    display: "flex",
-    alignItems: "center",
-  },
   panelHeader: {
     display: "flex",
     justifyContent: "space-between",
@@ -54,18 +47,5 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "2.625rem",
     lineHeight: "3.8125rem",
     fontWeight: 500,
-    margin: "1.875rem 0",
-  },
-  panelFooter: {
-    display: "flex",
-    justifyContent: "space-between",
-    "& .MuiTypography-body1": {
-      ...theme.typography.panelLinks,
-    },
-  },
-  linkIcon: {
-    width: "1.25rem",
-    height: "1.25rem",
-    color: theme.typography.panelLinks.color
   },
 }));
