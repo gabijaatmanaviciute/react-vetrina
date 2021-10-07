72ae79f13ae09c86bbd281f5c517a373b7b1111e
Gabija Atm <gab.atmanaviciute@gmail.com>
2021-10-07 09:01:21 +0300

    individual card components added

--- src/components/Page/Dashboard/MainPanel.js
+++ src/components/Page/Dashboard/MainPanel.js
@@ -2,17 +2,17 @@ import { Fragment } from "react";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import { makeStyles } from "@material-ui/core";
-import { ReactComponent as NewAppIcon } from "assets/icons/new-app-icon.svg";
-import { ReactComponent as FileTextIcon } from "assets/icons/file-text-icon.svg";
 import Visitors from "./PanelCards/Visitors";
 import Orders from "./PanelCards/Orders";
 import AppAd from "./PanelCards/AppAd";
+import ExtensionsMarketplace from "./PanelCards/ExtensionsMarketplace";
+import LatestNews from "./PanelCards/LatestNews";
 
 const MainPanel = () => {
   const classes = useStyles();
   return (
     <Fragment>
-      <Grid item container className={classes.gridRow}>
+      <Grid item container className={classes.gridRow1}>
         <Grid item className={classes.gridItem} md={6}>
           <Card className={classes.panelCard}>
             <Visitors />
@@ -24,9 +24,11 @@ const MainPanel = () => {
           </Card>
         </Grid>
       </Grid>
-      <Grid item container className={classes.gridRow}>
+      <Grid item container className={classes.gridRow2}>
         <Grid item className={classes.gridItem} md={6}>
-          <AppAd />
+          <Card className={classes.panelCard} id="app_ad">
+            <AppAd />
+          </Card>
         </Grid>
         <Grid item className={classes.gridItem} md={6}>
           <Card className={classes.panelCard}>
@@ -53,6 +55,7 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   panelCard: {
+    minHeight: "220px",
     ...theme.panelCard,
   },
   panelIcon: {
--- src/components/Page/Dashboard/SidePanel.js
+++ src/components/Page/Dashboard/SidePanel.js
@@ -2,11 +2,11 @@ import { Fragment } from "react";
 import Card from "@material-ui/core/Card";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core";
-import { ReactComponent as ToolIcon } from "assets/icons/tool-icon.svg";
-import { ReactComponent as UsersIcon } from "assets/icons/users-icon.svg";
-import { ReactComponent as HeadphonesIcon } from "assets/icons/headphones-icon.svg";
-import TrustPilotLogo from "assets/logo/trustpilot-logo.svg";
-import ArrowForwardRoundedIcon from '@material-ui/icons/ArrowForwardRounded';
+import CustomerSupport from "./PanelCards/CustomerSupport";
+import InviteFriend from "./PanelCards/InviteFriend";
+import ConfigureYourShop from "./PanelCards/ConfigureYourShop";
+import TrustPilot from "./PanelCards/Trustpilot";
+
 
 const SidePanel = () => {
   const classes = useStyles();
@@ -14,26 +14,22 @@ const SidePanel = () => {
     <Fragment>
       <Grid item className={classes.gridItem}>
         <Card className={classes.panelCard}>
-          <ToolIcon className={classes.panelIcon} />
-          <p>Side panel</p>
+        <ConfigureYourShop />
         </Card>
       </Grid>
       <Grid item className={classes.gridItem}>
         <Card className={classes.panelCard} id="trustpilot">
-          <img src={TrustPilotLogo} alt="Trustpilot logo" />
-          <p>Trustpilot</p>
+          <TrustPilot />
         </Card>
       </Grid>
       <Grid item className={classes.gridItem}>
         <Card className={classes.panelCard}>
-          <UsersIcon className={classes.panelIcon} />
-          <p>Invite friend</p>
+          <InviteFriend />
         </Card>
       </Grid>
       <Grid item className={classes.gridItem}>
         <Card className={classes.panelCard}>
-          <HeadphonesIcon className={classes.panelIcon} />
-          <p>Customer support</p>
+          <CustomerSupport />
         </Card>
       </Grid>
     </Fragment>
@@ -46,9 +42,8 @@ const useStyles = makeStyles((theme) => ({
   gridItem: {
     padding: "0.75rem",
     "& #trustpilot": {
-        background: "#000032",
-
-    }
+      background: "#000032",
+    },
   },
   panelCard: {
     ...theme.panelCard,
