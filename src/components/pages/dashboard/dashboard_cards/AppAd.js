a6223adc4c0d2209d42511e6b0821e3356a5897c
Gabija Atm <gab.atmanaviciute@gmail.com>
2021-10-13 03:51:29 +0300

    app ad style fixed

--- src/components/pages/dashboard/dashboard_cards/AppAd.js
+++ src/components/pages/dashboard/dashboard_cards/AppAd.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import { Grid } from "@material-ui/core";
+import { useTheme } from "@material-ui/core";
+import { Grid, Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
 import ArrowForwardRoundedIcon from "@material-ui/icons/ArrowForwardRounded";
 import Iphone12 from "assets/iPhone_12.svg";
@@ -10,37 +11,30 @@ import CardLink from "components/reusable_components/panel_card_parts/CardLink";
 
 function AppAd() {
   const classes = useStyles();
+  const theme = useTheme();
+
   return (
     <Grid container className={classes.panelContainer}>
-      <Grid item container className={classes.adUpperSection}>
-        <Grid item container className={classes.adAllText}>
-          <Grid item className={classes.adText}>
-            <Grid item>
-              <Typography variant="h3">
-                Sell your products on your exclusive APP published on the stores
-              </Typography>
-            </Grid>
-            <Grid item className={classes.adLink}>
-              <CardLink
-                linkText="Show more"
-                linkIcon={<ArrowForwardRoundedIcon />}
-              />
-            </Grid>
-          </Grid>
-          <Grid item>
-            <img
-              src={Iphone12}
-              alt="Iphone12 picture"
-              className={classes.iphone12}
-            />
-          </Grid>
-        </Grid>
+      <Grid item className={classes.adUpperSection}>
+        <Box className={classes.adAllText}>
+          <Typography variant="h3" component="p" className={classes.adText}>
+            Sell your products on your exclusive APP published on the stores
+          </Typography>
+          <CardLink
+            className={classes.adLink}
+            linkText="Show more"
+            linkIcon={<ArrowForwardRoundedIcon />}
+            linkColor="#fff"
+            href="https://play.google.com/store/apps/details?id=it.lastingdynamics.vetrinaliveadmin&gl=DE"
+          />
+        </Box>
+        <img src={Iphone12} alt="Iphone12" className={classes.iphone12} />
       </Grid>
       <Grid item container className={classes.appStoreLinks}>
-        <Grid>
+        <Grid item>
           <img src={AppStore} alt="App Store" />
         </Grid>
-        <Grid>
+        <Grid item>
           <img src={GooglePlay} alt="Google Play" />
         </Grid>
       </Grid>
@@ -55,16 +49,22 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     justifyContent: "space-between",
     color: theme.palette.common.white,
+    marginBottom: "1.75rem",
   },
   adAllText: {
-    // maxWidth: "55%",
+    padding: "0.625rem 1.4375rem 0 0"
   },
-  adText: {},
+  adText: {
+    display: "inline-block",
+    fontWeight: 400,
+    marginBottom: "0.8125rem",
+  },
   appStoreLinks: {
     display: "flex",
-    justifyContent: "space-between",
+    justifyContent: "space-around",
   },
   iphone12: {
+    display: "inline-block",
     width: "7.125rem",
     height: "14.25rem",
   },
